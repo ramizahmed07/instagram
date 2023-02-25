f81f53b778d1b06cfb144c108281ed49357a4bbe
Ramiz Ahmed <ramizahmedar@gmail.com>
2023-02-25 19:40:29 +0500

    Add seePost query

---
+++ users/posts/seePost/seePost.resolvers.js
@@ -0,0 +1,10 @@
+import client from "../../../client";
+
+export default {
+  Query: {
+    seePost: (_, { id }) =>
+      client.post.findUnique({
+        where: { id },
+      }),
+  },
+};
---
+++ users/posts/seePost/seePost.typeDefs.js
@@ -0,0 +1,5 @@
+export default `#graphql
+  type Query {
+    seePost(id: Int!): Post!
+  }
+`;
